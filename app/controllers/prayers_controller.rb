245301e6a6d54b4d137b3b149b24abdcf6427f7d
Joseph DiFrancesco <joseph.difrancesco@gmail.com>
2017-08-17 01:57:55 +0200

    prayer requests

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -1,2 +1,48 @@
 class PrayersController < ApplicationController
+def index
+    @prayer = Prayer.all
+  end
+
+  def show
+   @prayer = Prayer.find(params[:id])
+ end
+
+ def new
+  @prayer = Prayer.new
+end
+
+def create
+  @prayer = Prayer.new(prayer_params)
+  if @prayer.save
+    redirect_to prayers_path
+  else
+    render :new
+  end
+end
+
+def edit
+  @prayer = Prayer.find(params[:id])
+  @prayer.save
+end
+
+def update
+  @prayer = Prayer.find(params[:id])
+  @prayer.save
+  if @prayer.update(prayer_params)
+
+    redirect_to prayers_path
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @prayer = Prayer.find(params[:id])
+  @prayer.destroy
+  redirect_to prayers_path
+end
+
+def prayer_params
+  params.require(:prayer).permit(:id, :requestor,:recipient, :description, :date)
+end
 end
--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,2 +1,4 @@
 class Prayer < ApplicationRecord
+
 end
+
